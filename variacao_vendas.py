d6527da2bd1f40c95b95e7ca94ed3e1d68d119e5
João Vitor <jovijulio12@gmail.com>
2026-04-23 18:24:28 -0300

    Update variacao_vendas.py
    # Descrição:
    # Programa que calcula a variação percentual nas vendas entre dois anos
    # e sugere ações estratégicas com base no resultado obtido.

--- variacao_vendas.py
+++ variacao_vendas.py
@@ -1,17 +1,21 @@
-vend2022 = float(input("Quantidade de vendas 2022: "))
-vend2023 = float(input("Quantidade de vendas 2023: "))
+try:
+    vend2022 = float(input("Quantidade de vendas 2022: "))
+    vend2023 = float(input("Quantidade de vendas 2023: "))
 
-if vend2022 == 0:
-    print("Não é possível calcular a variação (divisão por zero).")
-else:
-    variacao = 100 * (vend2023 - vend2022) / vend2022
-    print(f"Variação: {variacao:.2f}%")
-
-    if variacao > 20:
-        print("Bonificação para o time de vendas")
-    elif 2 <= variacao <= 20:
-        print("Pequena bonificação para o time de vendas")
-    elif -10 <= variacao < 2:
-        print("Planejamento de políticas de incentivo às vendas.")
+    if vend2022 == 0:
+        print("Não é possível calcular a variação (divisão por zero).")
     else:
-        print("Corte de gastos.")
+        variacao = 100 * (vend2023 - vend2022) / vend2022
+        print(f"Variação: {variacao:.2f}%")
+
+        if variacao > 20:
+            print("Bonificação para o time de vendas")
+        elif 2 <= variacao <= 20:
+            print("Pequena bonificação para o time de vendas")
+        elif -10 <= variacao < 2:
+            print("Planejamento de políticas de incentivo às vendas.")
+        else:
+            print("Corte de gastos.")
+
+except ValueError:
+    print("Erro: digite apenas números válidos!")
